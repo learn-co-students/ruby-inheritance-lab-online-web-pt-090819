1d7e3aa68ea1a60df06e19bd0ceb4089ff7b4ea3
Seung Kim <seunglives@hotmail.com>
2019-10-12 02:36:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+  attr_accessor :first_name, :last_name
+  
+  
+  def initialize 
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end 
+  
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end 
+
+  def knowledge
+    @knowledge
+  end
 
 end
