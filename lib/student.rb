1f98743bd395bd0e0c51a9cb7391d2c06dad9b51
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-22 19:01:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User 
+  
+  attr_accessor :knowledge
+  
+  @knowledge = []
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def self.knowledge 
+    @knowledge
+  end
+  
 
 end
